c02975a30bde6579ccdef95d53c323df905ff23b
panedrone <sqldalmaker@gmail.com>
2022-05-28 10:16:34 +0300

    + Route.php

--- api/index.php
+++ api/index.php
@@ -8,20 +8,25 @@ require_once 'utils.php';
 
 require_once 'Route.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode('/', $uri);
+use Steampixel\Route;
 
-if (isset($uri[3])) {
-    $method = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_SPECIAL_CHARS);
-    $api = $uri[3];
-    if ($api === 'groups') {
-        handle_groups($uri, $method);
-        return;
-    } else if ($api === 'task') {
-        handle_task($uri, $method);
-        return;
-    }
-}
+$method = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_SPECIAL_CHARS);
+
+Route::add('/api/groups', function () {
+    handle_groups();
+}, ['get', 'post']);
+
+Route::add('/api/groups/([0-9]*)', function ($g_id) {
+    handle_group($g_id);
+}, ['get', 'put', 'delete']);
+
+Route::add('/api/groups/([0-9]*)/tasks', function ($g_id) {
+    handle_group_tasks($g_id);
+}, ['get', 'post']);
+
+Route::add('/api/task/([0-9]*)', function ($t_id) {
+    handle_task($t_id);
+}, ['get', 'put', 'delete']);
 
 // https://github.com/symfony/http-foundation/blob/5.4/Response.php
 
@@ -32,22 +37,31 @@ class Response
 
     const HTTP_BAD_REQUEST = 400;
     const HTTP_NOT_FOUND = 404;
+    const HTTP_METHOD_NOT_ALLOWED = 405;
 }
 
-header("HTTP/1.1 404 Not Found");
-http_response_code(Response::HTTP_NOT_FOUND);
+Route::methodNotAllowed(function ($path, $method) {
+    http_response_code(Response::HTTP_METHOD_NOT_ALLOWED);
+});
 
-function handle_groups($uri, $method)
+Route::pathNotFound(function ($path, $method) {
+    header("HTTP/1.1 404 Not Found");
+    http_response_code(Response::HTTP_NOT_FOUND);
+});
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri_parts = explode('/api/', $uri);
+if (count($uri_parts) > 0) {
+    $base = $uri_parts[0];
+} else {
+    $base = "/";
+}
+
+Route::run($base);
+
+function handle_groups()
 {
-    if (isset($uri[4])) {
-        $g_id = (int)$uri[4];
-        if (isset($uri[5]) && $uri[5] === 'tasks') {
-            handle_group_tasks($g_id, $method);
-        } else {
-            handle_group($g_id, $method);
-        }
-        return;
-    }
+    global $method;
     if ($method == "POST") {
         $data = json_decode(file_get_contents("php://input"));
         GroupsController::createGroup($data);
@@ -55,13 +69,12 @@ function handle_groups($uri, $method)
     } else if ($method == "GET") {
         $arr = GroupsController::getGroups();
         json_resp($arr);
-    } else {
-        http_response_code(Response::HTTP_BAD_REQUEST);
     }
 }
 
-function handle_group($g_id, $method)
+function handle_group($g_id)
 {
+    global $method;
     if ($method == "GET") {
         $item = GroupsController::readGroup($g_id);
         json_resp($item);
@@ -76,8 +89,9 @@ function handle_group($g_id, $method)
     }
 }
 
-function handle_group_tasks($g_id, $method)
+function handle_group_tasks($g_id)
 {
+    global $method;
     if ($method == "POST") {
         $data = json_decode(file_get_contents("php://input"));
         GroupTasksController::createTask($g_id, $data);
@@ -90,22 +104,18 @@ function handle_group_tasks($g_id, $method)
     }
 }
 
-function handle_task($uri, $method)
+function handle_task($t_id)
 {
-    if (isset($uri[4])) {
-        $t_id = (int)$uri[4];
-        if ($method == "GET") {
-            $item = TaskController::readTask($t_id);
-            json_resp($item);
-        } else if ($method == "PUT") {
-            $data = json_decode(file_get_contents("php://input"));
-            TaskController::updateTask($t_id, $data);
-        } else if ($method == "DELETE") {
-            TaskController::deleteTask($t_id);
-            http_response_code(Response::HTTP_NO_CONTENT);
-        } else {
-            http_response_code(Response::HTTP_BAD_REQUEST);
-        }
+    global $method;
+    if ($method == "GET") {
+        $item = TaskController::readTask($t_id);
+        json_resp($item);
+    } else if ($method == "PUT") {
+        $data = json_decode(file_get_contents("php://input"));
+        TaskController::updateTask($t_id, $data);
+    } else if ($method == "DELETE") {
+        TaskController::deleteTask($t_id);
+        http_response_code(Response::HTTP_NO_CONTENT);
     } else {
         http_response_code(Response::HTTP_BAD_REQUEST);
     }
