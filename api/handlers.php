4675c6595fd7f5847e6756f4f723823f59f87a0e
panedrone <sqldalmaker@gmail.com>
2022-05-28 20:53:27 +0300

    1.247 include_once __DIR__ . '/Task.php'

--- api/handlers.php
+++ api/handlers.php
@@ -8,19 +8,6 @@ require_once 'utils.php';
 
 $method = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_SPECIAL_CHARS);
 
-// https://github.com/symfony/http-foundation/blob/5.4/Response.php
-
-class Response
-{
-    const HTTP_CREATED = 201;
-    const HTTP_NO_CONTENT = 204;
-
-    const HTTP_BAD_REQUEST = 400;
-    const HTTP_NOT_FOUND = 404;
-    const HTTP_METHOD_NOT_ALLOWED = 405;
-}
-
-
 function handle_groups()
 {
     global $method;
--- api/utils.php
+++ api/utils.php
@@ -1,5 +1,18 @@
 <?php
 
+// https://github.com/symfony/http-foundation/blob/5.4/Response.php
+
+class Response
+{
+    const HTTP_CREATED = 201;
+    const HTTP_NO_CONTENT = 204;
+
+    const HTTP_BAD_REQUEST = 400;
+    const HTTP_NOT_FOUND = 404;
+    const HTTP_METHOD_NOT_ALLOWED = 405;
+}
+
+
 function json_resp($data)
 {
     header("Content-Type: application/json; charset=UTF-8");
