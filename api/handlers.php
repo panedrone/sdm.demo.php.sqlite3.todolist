bbfa51a6ec3f30ad4f0070a960e50954dcaeb96c
Andrii M <andrey.meretsky@jetsoftpro.com>
2023-03-19 08:46:34 +0200

    + vue.js

--- api/handlers.php
+++ api/handlers.php
@@ -10,61 +10,114 @@ $method = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_SPECIAL_C
 
 function handle_groups()
 {
-    global $method;
-    if ($method == "POST") {
-        $data = json_decode(file_get_contents("php://input"));
-        try {
+    try {
+        global $method;
+        if ($method == "POST") {
+            $data = json_decode(file_get_contents("php://input"));
+            if (json_last_error() != JSON_ERROR_NONE) {
+                http_response_code(Response::HTTP_BAD_REQUEST);
+                return;
+            }
+            if (!validate_group($data)) {
+                http_response_code(Response::HTTP_BAD_REQUEST);
+                return;
+            }
             GroupsController::createGroup($data);
             http_response_code(Response::HTTP_CREATED);
-        } catch (Exception $e) {
-            http_response_code(Response::HTTP_BAD_REQUEST);
+        } else if ($method == "GET") {
+            $arr = GroupsController::readGroups();
+            json_resp($arr);
         }
-    } else if ($method == "GET") {
-        $arr = GroupsController::readGroups();
-        json_resp($arr);
+    } catch (Exception $e) {
+        http_response_code(Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
 function handle_group($g_id)
 {
-    global $method;
-    if ($method == "GET") {
-        $item = GroupsController::readGroup($g_id);
-        json_resp($item);
-    } else if ($method == "PUT") {
-        $data = json_decode(file_get_contents("php://input"));
-        GroupsController::updateGroup($g_id, $data);
-    } else if ($method == "DELETE") {
-        GroupsController::deleteGroup($g_id);
-        http_response_code(Response::HTTP_NO_CONTENT);
+    try {
+        global $method;
+        if ($method == "GET") {
+            $item = GroupsController::readGroup($g_id);
+            json_resp($item);
+        } else if ($method == "PUT") {
+            $data = json_decode(file_get_contents("php://input"));
+            if (json_last_error() != JSON_ERROR_NONE) {
+                http_response_code(Response::HTTP_BAD_REQUEST);
+                return;
+            }
+            if (!validate_group($data)) {
+                http_response_code(Response::HTTP_BAD_REQUEST);
+                return;
+            }
+            GroupsController::updateGroup($g_id, $data);
+        } else if ($method == "DELETE") {
+            GroupsController::deleteGroup($g_id);
+            http_response_code(Response::HTTP_NO_CONTENT);
+        }
+    } catch (Exception $e) {
+        http_response_code(Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
 function handle_group_tasks($g_id)
 {
-    global $method;
-    if ($method == "POST") {
-        $data = json_decode(file_get_contents("php://input"));
-        GroupTasksController::createTask($g_id, $data);
-        http_response_code(Response::HTTP_CREATED);
-    } else if ($method == "GET") {
-        $arr = GroupTasksController::readGroupTasks($g_id);
-        json_resp($arr);
+    try {
+        global $method;
+        if ($method == "POST") {
+            $data = json_decode(file_get_contents("php://input"));
+            if (json_last_error() != JSON_ERROR_NONE) {
+                http_response_code(Response::HTTP_BAD_REQUEST);
+                return;
+            }
+            if (!validate_task($data)) {
+                http_response_code(Response::HTTP_BAD_REQUEST);
+                return;
+            }
+            GroupTasksController::createTask($g_id, $data);
+            http_response_code(Response::HTTP_CREATED);
+        } else if ($method == "GET") {
+            $arr = GroupTasksController::readGroupTasks($g_id);
+            json_resp($arr);
+        }
+    } catch (Exception $e) {
+        http_response_code(Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
 function handle_task($t_id)
 {
-    global $method;
-    if ($method == "GET") {
-        $item = TaskController::readTask($t_id);
-        json_resp($item);
-    } else if ($method == "PUT") {
-        $data = json_decode(file_get_contents("php://input"));
-        TaskController::updateTask($t_id, $data);
-    } else if ($method == "DELETE") {
-        TaskController::deleteTask($t_id);
-        http_response_code(Response::HTTP_NO_CONTENT);
+    try {
+        global $method;
+        if ($method == "GET") {
+            $item = TaskController::readTask($t_id);
+            json_resp($item);
+        } else if ($method == "PUT") {
+            $data = json_decode(file_get_contents("php://input"));
+            if (json_last_error() != JSON_ERROR_NONE) {
+                http_response_code(Response::HTTP_BAD_REQUEST);
+                return;
+            }
+            if (!validate_task($data)) {
+                http_response_code(Response::HTTP_BAD_REQUEST);
+                return;
+            }
+            TaskController::updateTask($t_id, $data);
+        } else if ($method == "DELETE") {
+            TaskController::deleteTask($t_id);
+            http_response_code(Response::HTTP_NO_CONTENT);
+        }
+    } catch (Exception $e) {
+        http_response_code(Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
 
+function validate_group($data): bool
+{
+    return true; // TODO
+}
+
+function validate_task($data): bool
+{
+    return true; // TODO
+}
--- api/utils.php
+++ api/utils.php
@@ -10,6 +10,7 @@ class Response
     const HTTP_BAD_REQUEST = 400;
     const HTTP_NOT_FOUND = 404;
     const HTTP_METHOD_NOT_ALLOWED = 405;
+    const HTTP_INTERNAL_SERVER_ERROR = 500;
 }
 
 
